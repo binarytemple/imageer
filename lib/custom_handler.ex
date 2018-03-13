705994bd012f30283b9010074832aca3d36544c2
bryanhuntesl <bryan.hunt@erlang-solutions.com>
2018-03-13 20:57:46 +0000

    tidy/accumulate file

--- lib/custom_handler.ex
+++ lib/custom_handler.ex
@@ -3,45 +3,48 @@ defmodule Imageer.CustomHandler do
 
   def init(_type, req, []) do
     {:ok, req, :no_state}
-end
+  end
 
-def handle(request, state) do
-  { :ok, reply } = :cowboy_req.reply(
+  def handle(request, state) do
+    warn("cowboy_req.has_body(request) #{:cowboy_req.has_body(request)}")
 
-    # status code
-    200,
+    if :cowboy_req.has_body(request) do
+      file=stream_body(request, "")
+      info("file contents \n#{inspect(file)}")
+    end
 
-    # headers
-    [ {"content-type", "text/html"} ],
+    Logger.warn("got this far")
 
-    # body of reply.
-    "hello world",
+    {:ok, resp} =
+      :cowboy_req.reply(200, [{"content-type", "text/plain"}], "Hello world!", request)
 
-    # original request
-    request
-)
-{:ok, reply, state}
-end
-
-  # def handle(req, state) do
-  #   Logger.warn(req, state)
-  #    x  = :cowboy_req.reply(200, [ {"content-type", "text/plain"} ], "Hello world!", req)
-  #   # warn(x)
-
-  #   #  {:ok, req2} = x
-  #   {:ok, req, state}
-  # end
+    {:ok, resp, state}
+  end
 
   def terminate(_Reason, _Req, _State) do
-    Logger.warn("terminate")
+    Logger.warn("terminating")
     :ok
   end
-end
 
-# stream_body(Req0, Acc) ->
-#  case cowboy_req:read_part_body(Req0) of
-#      {more, Data, Req} ->
-#          stream_body(Req, << Acc/binary, Data/binary >>);
-#      {ok, Data, Req} ->
-#          {ok, << Acc/binary, Data/binary >>, Req}
-#  end.
+  @chunk 8
+  @read_length 1024 * @chunk
+  @load_length 1024 * @chunk
+
+  def stream_body(req0, acc) do
+    warn("entering into :  stream_body(req0, ")
+    case :cowboy_req.body(req0, [
+        { :length, @load_length},
+        { :read_length, @read_length}
+      ] ) do
+      {:ok,data, req} ->
+        warn("entering into :  ok - size : #{:erlang.size(data)}"  )
+        {:ok, <<acc::bitstring,data::bitstring >> ,  req}
+      {:more, data, req} ->
+        warn("entering into :  more - size : #{:erlang.size(data)}")
+          stream_body(req, <<acc::bitstring,data::bitstring >>    )
+      {:error, reason} ->
+        error(reason)
+        {:ok, "final", req0}
+    end
+  end
+end
